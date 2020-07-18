d4b2b33c8be22025751145b827498c54185955a2
Keri Southwood-Smith <9258461+KSouthwood@users.noreply.github.com>
2020-07-18 16:26:59 -0400

    [feat] complete Stage 2
    Game starts with an empty board, with user making the first move versus the computer. The computer just makes random moves using the Random class. Game continues until there's a winner or no more moves are left.
    
    Factored the board class out into its own file.

--- Main.java
+++ Main.java
@@ -1,24 +1,35 @@
 package tictactoe;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter cells: ");
-        String board = input.next();
-        TTTBoard gameBoard = new TTTBoard(board);
+        TTTBoard gameBoard = new TTTBoard();
         gameBoard.printBoard();
-        while (true) {
-            if (gameBoard.play(getMove())) {
+        int move = 0;
+        do {
+            switch (move) {
+                case 0:
+                if (gameBoard.play(humanPlayer())) {
+                    move = (move + 1) % 2;
+                } else {
+                    System.out.println("This cell is occupied! Choose another one!");
+                }
                 break;
+                case 1:
+                    if (gameBoard.play(computerPlayer())) {
+                        System.out.println("Making move level \"easy\"");
+                        move = (move + 1) % 2;
+                    }
             }
-        }
-        gameBoard.printBoard();
+            gameBoard.printBoard();
+        } while (gameBoard.boardStatus().equals(TTTBoard.BoardStates.NOT_DONE.getMessage()));
+
         System.out.println(gameBoard.boardStatus());
     }
 
-    private static int getMove() {
+    private static int humanPlayer() {
         Scanner input = new Scanner(System.in);
         int col = 0;
         int row = 0;
@@ -57,102 +68,15 @@ public class Main {
 
         return 6 + (col - 1) - (3 * (row - 1));
     }
-}
 
-class TTTBoard {
-    private final char[] board;
-    private final static char EDGE = '|';
-    private final static String BORDER = "---------";
-    private final static int[][] lines = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
-            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
-    private int countX;
-    private int countO;
+    private static int computerPlayer() {
+        Random row = new Random();
+        Random col = new Random();
 
-    private enum BoardStates {
-        DRAW("Draw"),
-        XWON("X wins"),
-        OWON("O wins"),
-        NOT_DONE("Game not finished");
-
-        private final String message;
-
-        BoardStates(String msg) {
-            this.message = msg;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-    }
-
-//    TTTBoard() {
-//        new TTTBoard("_________");
-//    }
-
-    TTTBoard(String board) {
-        this.board = board.toCharArray();
-
-        for (int i = 0; i < board.length(); i++) {
-            if (this.board[i] == 'X') {
-                countX++;
-            }
-            if (this.board[i] == 'O') {
-                countO++;
-            }
-        }
-    }
-
-    public void printBoard() {
-        System.out.println(BORDER);
-        System.out.printf("%s %s %s %s %s\n", EDGE, board[0], board[1], board[2], EDGE);
-        System.out.printf("%s %s %s %s %s\n", EDGE, board[3], board[4], board[5], EDGE);
-        System.out.printf("%s %s %s %s %s\n", EDGE, board[6], board[7], board[8], EDGE);
-        System.out.println(BORDER);
-    }
-
-    public String boardStatus() {
-        BoardStates state;
-        int i = 0;
-
-        do {
-            state = checkForWinner(lines[i]);
-            i++;
-        } while (state == BoardStates.NOT_DONE && i < 8);
-
-        if (state == BoardStates.NOT_DONE) {
-            if (!String.valueOf(board).contains("_")) {
-                state = BoardStates.DRAW;
-            }
-        }
-
-        return state.getMessage();
-    }
-
-    private BoardStates checkForWinner(int[] line) {
-        BoardStates state = BoardStates.NOT_DONE;
-
-        if ((board[line[0]] == board[line[1]]) && (board[line[1]] == board[line[2]])) {
-            if (board[line[0]] != '_') {
-                state = (board[line[0]] == 'X') ? BoardStates.XWON : BoardStates.OWON;
-            }
-        }
-
-        return state;
-    }
-
-    public boolean play(int move) {
-        if (board[move] == '_') {
-            if (countO == countX) {
-                board[move] = 'X';
-                countX++;
-            } else {
-                board[move] = 'O';
-                countO++;
-            }
-            return true;
-        }
-
-        System.out.println("This cell is occupied! Choose another one!");
-        return false;
+//        do {
+//            int r = row(3) + 1;
+//            int c = col(3) + 1;
+//        }
+        return 6 + col.nextInt(3) - (3 * (row.nextInt(3)));
     }
 }
---
+++ TTTBoard.java
@@ -0,0 +1,100 @@
+package tictactoe;
+
+public class TTTBoard {
+    private char[] board;
+    private final static char EDGE = '|';
+    private final static String BORDER = "---------";
+    private final static int[][] lines = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+    private int countX;
+    private int countO;
+
+    public enum BoardStates {
+        DRAW("Draw"),
+        XWON("X wins"),
+        OWON("O wins"),
+        NOT_DONE("Game not finished");
+
+        private final String message;
+
+        BoardStates(String msg) {
+            this.message = msg;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+
+    TTTBoard() {
+        this.board = "_________".toCharArray();
+        countO = 0;
+        countX = 0;
+    }
+
+//    TTTBoard(String board) {
+//        this.board = board.toCharArray();
+//
+//        for (int i = 0; i < board.length(); i++) {
+//            if (this.board[i] == 'X') {
+//                countX++;
+//            }
+//            if (this.board[i] == 'O') {
+//                countO++;
+//            }
+//        }
+//    }
+
+    public void printBoard() {
+        System.out.println(BORDER);
+        System.out.printf("%s %s %s %s %s\n", EDGE, board[0], board[1], board[2], EDGE);
+        System.out.printf("%s %s %s %s %s\n", EDGE, board[3], board[4], board[5], EDGE);
+        System.out.printf("%s %s %s %s %s\n", EDGE, board[6], board[7], board[8], EDGE);
+        System.out.println(BORDER);
+    }
+
+    public String boardStatus() {
+        BoardStates state;
+        int i = 0;
+
+        do {
+            state = checkForWinner(lines[i]);
+            i++;
+        } while (state == BoardStates.NOT_DONE && i < 8);
+
+        if (state == BoardStates.NOT_DONE) {
+            if (!String.valueOf(board).contains("_")) {
+                state = BoardStates.DRAW;
+            }
+        }
+
+        return state.getMessage();
+    }
+
+    private BoardStates checkForWinner(int[] line) {
+        BoardStates state = BoardStates.NOT_DONE;
+
+        if ((board[line[0]] == board[line[1]]) && (board[line[1]] == board[line[2]])) {
+            if (board[line[0]] != '_') {
+                state = (board[line[0]] == 'X') ? BoardStates.XWON : BoardStates.OWON;
+            }
+        }
+
+        return state;
+    }
+
+    public boolean play(int move) {
+        if (board[move] == '_') {
+            if (countO == countX) {
+                board[move] = 'X';
+                countX++;
+            } else {
+                board[move] = 'O';
+                countO++;
+            }
+            return true;
+        }
+
+        return false;
+    }
+}
